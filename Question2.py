6b6729ccace269c096100bd949bb13c0d36c3213
Trevor Yovaish <tyovaish13@gmail.com>
2017-08-19 17:38:33 -0400

    added finding wins and losses

--- Question2.py
+++ Question2.py
@@ -5,14 +5,17 @@ class Team:
         self.divisionName=divisionName
         self.conferenceName=conferenceName
         self.gamesPlayedByTeam=[]
-        self.winLossGeneral = 0
+        self.winGeneral = 0
+        self.lossGeneral= 0
         self.winLossD = {}
-        self.homeScore = 0
-        self.awayScore = 0
-        self.homeRecord = 0
-        self.awayRecord = 0
     def addGame(self,game):
         self.gamesPlayedByTeam.append(game)
+        if game.isWinner(self):
+            self.winGeneral += 1
+        else:
+            self.lossGeneral += 1
+    def printData(self):
+        print(self.teamName,' ',str(self.winGeneral),'-',str(self.lossGeneral))
 
 class Game:
     def __init__(self,date,homeTeam, awayTeam, homeScore,awayScore):
@@ -21,15 +24,26 @@ class Game:
         self.awayTeam=awayTeam
         self.homeScore=homeScore
         self.awayScore=awayScore
-        homeTeam.addGame(self)
-        awayTeam.addGame(self)
     def isWinner(self,team):
         if self.homeTeam == team and self.homeScore>self.awayScore:
             return True
-        else:
-            return False
+        if self.awayTeam==team and self.awayScore>self.homeScore:
+            return True
+        return False
+    def printData(self):
+        print(self.homeTeam.teamName, ' ', self.awayTeam.teamName)
+
+
 division_Info=pandas.read_csv("Division_Info.csv")
 nba_Season=pandas.read_csv("NBA_2016_2017_Scores.csv")
 teams = {}
 for index, row in division_Info.iterrows():
     teams[row[0]] = Team(row[0],row[1],row[2])
+
+for index, row in nba_Season.iterrows():
+    gamePlayed=Game(row[0],teams[row[1]],teams[row[2]],row[3],row[4])
+    teams[row[1]].addGame(gamePlayed)
+    teams[row[2]].addGame(gamePlayed)
+
+for teamName,teamData in teams.items():
+    teamData.printData()
